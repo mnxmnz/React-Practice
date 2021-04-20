af39d215ccc89b0bce7a20bde269e9118b8b9448
mnxmnz <codi0703@naver.com>
2021-04-20 22:01:56 +0900

    12. useRef로 컴포넌트 안의 변수 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import InputSample from './InputSample';
+import React, { useRef } from 'react';
+import UserList from './UserList';
 
 function App() {
-  return <InputSample />;
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
